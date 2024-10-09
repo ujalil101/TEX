ee17a8f1a577a7f804076e2deeadba7b4dac755f
Usman Jalil <alice@webage.com>
2024-10-09 19:29:18 -0400

    fixed ui

---
+++ react/src/components/SearchDirectory.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import EmployeeCard from './EmployeeCard';
+import './SearchDirectory.css';
+
+const SearchDirectory = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [userInfo, setUserInfo] = useState([]);
+  const [message, setMessage] = useState('');
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`http://localhost:3000/employees?name=${searchQuery}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setUserInfo(data);
+        setMessage('');
+      } else {
+        setMessage('Employee not found');
+        setUserInfo([]);
+      }
+    } catch (error) {
+      setMessage('Internal server error');
+      setUserInfo([]);
+    }
+  };
+
+  return (
+    <div className="search-directory">
+      <form className="search-form" onSubmit={handleSearch}>
+        <div className="search-input-container">
+          <h3>Directory</h3>
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            required
+          />
+          <button type="submit">Search</button>
+        </div>
+      </form>
+      <div className="employee-info">
+        {message && <p>{message}</p>}
+        {userInfo.length > 0 && (
+          <div className="employee-list">
+            <h3>Employee Information</h3>
+            <div className="employee-cards-container">
+              {userInfo.map((employee) => (
+                <EmployeeCard key={employee._id} employee={employee} />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SearchDirectory;
---
+++ react/src/components/Welcome.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Welcome.css';
+
+const Welcome = () => {
+  return (
+    <div className="welcome-container">
+      <h3>Welcome to TEX</h3>
+      <h2>Travelers Explore EXplorer</h2>
+      <p>TEX offers Trav Employees  with the compalitiy to search the enterprise-wide employee directory! </p>
+    </div>
+  );
+};
+
+export default Welcome;
