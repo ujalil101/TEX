3819457719fc29eca51d2f0b48987ffd474da8f2
Usman Jalil <alice@webage.com>
2024-10-10 09:57:53 -0400

    finsihed ui

--- react/src/components/EmployeeCard.jsx
+++ react/src/components/EmployeeCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import './EmployeeCard.css'; 
+import './EmployeeCard.css';
 
-const EmployeeCard = ({ employee }) => {
+const EmployeeCard = ({ employee, showSalary }) => {
   return (
     <div className="employee-card">
       <p><strong>Name:</strong> {employee.name}</p>
       <p><strong>Role:</strong> {employee.jobRole}</p>
       <p><strong>Location:</strong> {employee.workLocation}</p>
-      <p><strong>Salary:</strong> {employee.salary}</p>
+      {showSalary && <p><strong>Salary:</strong> {employee.salary}</p>}
     </div>
   );
 };
