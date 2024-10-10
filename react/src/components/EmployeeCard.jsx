840eccf3bf5b8f5167e877fc98fe17b6b2370304
Usman Jalil <alice@webage.com>
2024-10-10 11:59:15 -0400

    updated repo

--- react/src/components/EmployeeCard.jsx
+++ react/src/components/EmployeeCard.jsx
@@ -7,6 +7,7 @@ const EmployeeCard = ({ employee, showSalary }) => {
       <p><strong>Name:</strong> {employee.name}</p>
       <p><strong>Role:</strong> {employee.jobRole}</p>
       <p><strong>Location:</strong> {employee.workLocation}</p>
+      <p><strong>Phone Number:</strong> {employee.phoneNumber}</p>
       {showSalary && <p><strong>Salary:</strong> ${employee.salary.toLocaleString()}</p>}
     </div>
   );
